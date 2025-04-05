0dfc8cc75aad9fdc456a27ea896007d3d88418a4
MeetRadadiya1999 <meetradadiya45@gmail.com>
2025-04-05 14:30:28 +0530

    update db file for db connection

--- config/db.js
+++ config/db.js
@@ -2,14 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, { 
-      useNewUrlParser: true, 
-    //   useUnifiedTopology: true 
-    });
+    await mongoose.connect(process.env.MONGO_URI); // No need for deprecated options
     console.log("✅ MongoDB Connected");
   } catch (error) {
     console.error("❌ MongoDB Connection Failed:", error);
-    process.exit(1);
+    process.exit(1); // Exit app if DB connection fails
   }
 };
 
